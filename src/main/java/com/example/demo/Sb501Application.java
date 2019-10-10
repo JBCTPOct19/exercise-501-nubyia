c96de39f9dc2ea3985b11801985e98fc7a7c3a8b
nubyia <shareenca@gmail.com>
2019-10-10 12:01:52 -0400

    Ex501

---
+++ src/main/java/com/example/demo/Sb501Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb501Application.class, args);
+    }
+
+}
